bd32efd0fc83c05b480330c4b4a7c7c99160d824
bykova_iuliia <lktisw@yandex.ru>
2020-12-14 21:34:17 +0300

    v1

---
+++ scripts/scroll.js
@@ -0,0 +1,7 @@
+let datas = document.querySelectorAll('.choices__list--dropdown');
+
+new SimpleBar(datas[0]);
+new SimpleBar(datas[1]);
+new SimpleBar(datas[2]);
+new SimpleBar(datas[3]);
+new SimpleBar(datas[4]);
