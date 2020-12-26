29a2b0a4acfb682ff003d50dac906d770f562aa0
bykova_iuliia <lktisw@yandex.ru>
2020-12-26 19:15:13 +0300

    v.4.1

--- scripts/accordion.js
+++ scripts/accordion.js
@@ -1,42 +1,60 @@
-(function() {
-	
 let headings = document.querySelectorAll('.accordion__heading');
+let accordion = document.querySelector('.accordion');
 
-for(let i = 0; i < headings.length; i++) {
-	headings[i].addEventListener('click', openCurrAccordion);
+for (let i = 0; i < headings.length; i++) {
+  headings[i].addEventListener('click', openCurrAccordion);
 }
 
+for (let i = 0; i < headings.length; i++) {
+	headings[i].addEventListener('keyup', enterAccordion);
+  }
+
+
 //Если нужно, чтобы открывалось несколько списков
-function openAccordion(e) {
-	let parent = this.parentElement;
-	let article = this.nextElementSibling;
-	
-	if (!parent.classList.contains('open')) {
-		parent.classList.add('open');
-		article.style.maxHeight = article.scrollHeight + 'px';
-	}
-	else {
-		parent.classList.remove('open');
-		article.style.maxHeight = '0px';
-	}
+function openAccordion(event) {
+  let parent = this.parentElement;
+  let article = this.nextElementSibling;
+
+  if (!parent.classList.contains('open')) {
+    parent.classList.add('open');
+    article.style.maxHeight = article.scrollHeight + 'px';
+  } else {
+    parent.classList.remove('open');
+    article.style.maxHeight = '0px';
+  }
 }
 
 //Если нужно, чтобы открывался один
 
-function openCurrAccordion(e) {
-	for(let i = 0; i < headings.length; i++) {
-		let parent = headings[i].parentElement;
-		let article = headings[i].nextElementSibling;
+function openCurrAccordion(event) {
+  for (let i = 0; i < headings.length; i++) {
+    let parent = headings[i].parentElement;
+    let article = headings[i].nextElementSibling;
 
-		if (this === headings[i] && !parent.classList.contains('open')) {
-			parent.classList.add('open');
-			article.style.maxHeight = article.scrollHeight + 'px';
-		}
-		else {
-			parent.classList.remove('open');
-			article.style.maxHeight = '0px';
-		}
-	}
+    if (this === headings[i] && !parent.classList.contains('open')) {
+      parent.classList.add('open');
+      article.style.maxHeight = article.scrollHeight + 'px';
+    } else {
+      parent.classList.remove('open');
+      article.style.maxHeight = '0px';
+    }
+  }
 }
 
-})();
+ function enterAccordion(event) {
+	focused = document.querySelector(":focus");
+	if ((event.key == 'Tab') && focused.classList.contains('accordion__subtitle')) {
+	  let parent = focused.parentNode.parentNode;
+	  let article =  parent.querySelector('.accordion__body');
+
+	  this.addEventListener('keydown', (event) => {
+		if ((event.key == 'Enter') && (!parent.classList.contains('open'))  ) {
+		  parent.classList.add('open');
+		  article.style.maxHeight = article.scrollHeight + 'px';
+		} else {
+		  parent.classList.remove('open');
+		  article.style.maxHeight = '0px';
+		}
+	  })
+	}
+ }
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,27 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
+let tabBtnAll = document.querySelectorAll('.tabs__btn');
 
-  let tabBtnAll =  document.querySelectorAll('.tabs__btn');
+tabBtnAll.forEach(function (tabsBtn) {
 
-  tabBtnAll.forEach(function (tabsBtn) {
-
-    tabsBtn.addEventListener('click', function (event) {
-      tabBtnAll.forEach(function (tabsBtn) {
-        if (tabsBtn.parentElement.classList.contains('tabs__item-active')) {
-          tabsBtn.parentElement.classList.remove('tabs__item-active');
-        }; 
-      })
-      const path = event.currentTarget.dataset.path;  
-      tabsBtn.parentElement.classList.add('tabs__item-active');
-
-      document.querySelectorAll('.tabs__content').forEach((tabContent) => {
-        if (tabContent.classList.contains('tabs__content-active')) {
-          tabContent.classList.remove('tabs__content-active');
-        }
-      })
-
-      let elem = document.querySelector(`[data-target="${path}"]`);
-      elem.classList.add('tabs__content-active');
+  tabsBtn.addEventListener('click', function (event) {
+    openCurrAccordion();
+    tabBtnAll.forEach(function (tabsBtn) {
+      if (tabsBtn.parentElement.classList.contains('tabs__item-active')) {
+        tabsBtn.parentElement.classList.remove('tabs__item-active');
+      };
     })
+    const path = event.currentTarget.dataset.path;
+    tabsBtn.parentElement.classList.add('tabs__item-active');
 
+    document.querySelectorAll('.accordion__inner').forEach((accContent) => {
+      if (accContent.classList.contains('accordion__inner-active')) {
+        accContent.classList.remove('accordion__inner-active');
+      }
+    })
+    
+    let elemAll = document.querySelectorAll(`[data-target="${path}"]`);
+     
+    for (let i=0; i < elemAll.length; i++) {
+      elemAll[i].classList.add('accordion__inner-active');
+    }
   })
+
 })
+
+
